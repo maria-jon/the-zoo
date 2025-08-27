551a925ec655ebd358cf7eb07d49ed529b67432a
maria-jon <mariajonasson93@gmail.com>
2025-08-27 14:44:06 +0200

    :sparkles: ApiAnimal type

---
+++ src/models/ApiAnimal.ts
@@ -0,0 +1,12 @@
+export type ApiAnimal = {
+    id: string;
+    name: string;
+    latinName: string;
+    yearOfBirth: number;
+    shortDescription: string;
+    longDescription: string;
+    imageUrl: string;
+    medicine: string;
+    isFed: boolean;
+    lastFed: string;
+};
