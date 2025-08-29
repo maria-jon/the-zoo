17f7a2865ea903b78f2e6536753ae030086601ad
maria-jon <mariajonasson93@gmail.com>
2025-08-29 16:18:23 +0200

    :sparkles: add info to start page

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,7 +1,31 @@
 export const Start = () => {
     return(
         <>
-            <section>Startsida</section>
+            <section className="p-4 rounded-xl border-2 border-teal-700">
+                <h2 className="text-4xl mb-6">Välkommen till Zoo-Bop-a-Lula!</h2>
+                <p>Hos oss får du komma riktigt nära djuren - klappa, mata och upptäcka deras unika personligheter. Från mjuka kaniner till nyfikna illrar väntar oförglömliga möten för hela familjen. Kom och upplev en dag fylld av lek, lärande och kärlek till djuren!</p>
+            </section>
+            <section className="p-4 rounded-xl border-2 border-teal-700 mt-12">
+                <h3 className="text-3xl mb-4">Aktiviteter på Zoo-Bop-a-Lula</h3>
+                <ul className="list-inside list-disc">
+                    <li>Klappa och mata djuren i kontaktgården</li>
+                    <li>Ge illrar och kaniner deras favoritgodis</li>
+                    <li>Följ med på matstunder och lär dig mer om djuren</li>
+                    <li>Guidad tur med roliga fakta och berättelser</li>
+                    <li>Pysselhörna och lekfulla aktiviteter för barn</li>
+                </ul>
+            </section>
+
+            <section className="p-4 rounded-xl border-2 border-teal-700 mt-12">
+                <h3 className="text-3xl mb-4">Öppettider</h3>
+                <p>Måndag - stängt</p>
+                <p>Tisdag - 10.00-16.00</p>
+                <p>Onsdag - 10.00-18.00</p>
+                <p>Torsdag - 10.00-18.00</p>
+                <p>Fredag - 10.00-19.00</p>
+                <p>Lördag - 10.00-19.00</p>
+                <p>Söndag - 11.00-19.00</p>
+            </section>
         </>
     )
 }
